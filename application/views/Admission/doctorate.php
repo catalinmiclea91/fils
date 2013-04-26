3577432f9ed6cc99055f7a26131a948c9b2c607a
laitz laitz <ciprian.popescu1991@gmail.com>
2013-04-26 04:51:32 +0300

    addmission pages

--- application/views/Admission/doctorate.php
+++ application/views/Admission/doctorate.php
@@ -1,6 +1,19 @@
-<div>
-    &nbsp;Testing... 1, 2, testing <h1>doctorate</h1>&nbsp;&nbsp;&nbsp;
-    <input name="Submit1" style="height: 26px" type="submit" value="submit">
-    <form method="post">
-    </form>
+<div class='information'></div>
+<div class='documents'>
+    <div class='line-item'>
+        <i class='icon-pdf'></i>
+        <a href='/upload/docs/pdf_doc.pdf'>Admitere 2012 studenti straini erasmus</a>
+    </div><br>
+
+
+    <div class='line-item'>
+        <i class=ícon-word></i>
+        <a href='/upload/docs/word_doc.pdf'>Admitere 2012 studenti straini erasmus</a>
+    </div><br>
+
+    <div class='line-item'>
+        <i class=ícon-excel'></i>
+        <a href='/upload/docs/excel_doc.pdf'>Admitere 2012 studenti straini erasmus</a>
+    </div>
 </div>
+<div class='repartition'></div>
