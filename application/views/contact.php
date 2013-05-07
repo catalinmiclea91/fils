a59d974404eecc58d0632c362e224cfb1802f0c0
Catalin miclea <catalinmiclea91@gmail.com>
2013-05-08 01:19:42 +0300

    Nu e gata

--- application/views/contact.php
+++ application/views/contact.php
@@ -1,6 +1,30 @@
-<div>
-    &nbsp;Testing... 1, 2, testing <h1>contact</h1>&nbsp;&nbsp;&nbsp;
-    <input name="Submit1" style="height: 26px" type="submit" value="submit">
-    <form method="post">
-    </form>
+<div class="contact-main clean">
+    <div class="contact-titles">
+        <div class="contact-titles-map clean">
+            <p class="clean">Unde ne gasiti:</p>
+        </div>
+        <div class="contact-titles-info clean">
+            <p class="clean">Detalii contact:</p>
+        </div>
+    </div>
+    <div class="contact-map-wrapper">
+        <div class="contact-map">
+            <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=fils+bucuresti&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=60.158465,135.263672&amp;t=h&amp;ie=UTF8&amp;hq=fils&amp;hnear=Bucharest,+Romania&amp;ll=44.439525,26.050172&amp;spn=0.016031,0.112666&amp;output=embed&iwloc=near"></iframe>
+            <br />
+            <small>
+                <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=fils+bucuresti&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=60.158465,135.263672&amp;t=h&amp;ie=UTF8&amp;hq=fils&amp;hnear=Bucharest,+Romania&amp;ll=44.439525,26.050172&amp;spn=0.016031,0.112666" style="color:#527787;text-align:left;font-size:14px;">Vezi harta marita</a>
+            </small>
+        </div>
+    </div>
+    <div class="contact-info-wrapper">
+        <div class="contact-info">
+            <p class="fac">Facultatea de inginerie in limbi straine</p>
+            <p class="upb">Universitatea POLITEHNICA din Bucuresti</p>
+            <p><span class="det">Adresa:</span><small> Splaiul Independentei 313, Bucuresti, Cod Postal 060042, ROMANIA</small></p>
+            <p class="clean"><span class="det">Secretariat:</span><small> sala JE 105</small></p>
+            <p class="clean"><span class="detMic">Telefon:</span><small> +4021 - 4029889</small></p>
+            <p class="clean"><span class="detMic">Fax:</span><small> +4021 - 4029111</small></p>
+        </div>
+    </div>
 </div>
+<div class="clearfix"></div>
