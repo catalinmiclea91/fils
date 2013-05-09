ec5731094e4c904d1da95c9237c371090281a219
Florian Popa <florian@codeforge.ro>
2013-05-09 03:13:54 +0300

    fonts

--- application/views/Education/doctorate.php
+++ application/views/Education/doctorate.php
@@ -1,6 +1,21 @@
 <div>
-    &nbsp;Testing... 1, 2, testing <h1>doctorate</h1>&nbsp;&nbsp;&nbsp;
-    <input name="Submit1" style="height: 26px" type="submit" value="submit">
-    <form method="post">
-    </form>
+    <p>
+        Lorem Ipsum este pur şi simplu o machetă pentru text a industriei tipografice. Lorem Ipsum a fost macheta standard a industriei încă din secolul al XVI-lea, când un tipograf anonim a luat o planşetă de litere şi le-a amestecat pentru a crea o carte demonstrativă pentru literele respective. Nu doar că a supravieţuit timp de cinci secole, dar şi a facut saltul în tipografia electronică practic neschimbată. A fost popularizată în anii '60 odată cu ieşirea colilor Letraset care conţineau pasaje Lorem Ipsum, iar mai recent, prin programele de publicare pentru calculator, ca Aldus PageMaker care includeau versiuni de Lorem Ipsum.
+    </p>
+
+
+    <p>
+        Lorem Ipsum este pur şi simplu o machetă pentru text a industriei tipografice. Lorem Ipsum a fost macheta standard a industriei încă din secolul al XVI-lea, când un tipograf anonim a luat o planşetă de litere şi le-a amestecat pentru a crea o carte demonstrativă pentru literele respective. Nu doar că a supravieţuit timp de cinci secole, dar şi a facut saltul în tipografia electronică practic neschimbată. A fost popularizată în anii '60 odată cu ieşirea colilor Letraset care conţineau pasaje Lorem Ipsum, iar mai recent, prin programele de publicare pentru calculator, ca Aldus PageMaker care includeau versiuni de Lorem Ipsum.
+    </p>
+
+
+    <p>
+        Lorem Ipsum este pur şi simplu o machetă pentru text a industriei tipografice. Lorem Ipsum a fost macheta standard a industriei încă din secolul al XVI-lea, când un tipograf anonim a luat o planşetă de litere şi le-a amestecat pentru a crea o carte demonstrativă pentru literele respective. Nu doar că a supravieţuit timp de cinci secole, dar şi a facut saltul în tipografia electronică practic neschimbată. A fost popularizată în anii '60 odată cu ieşirea colilor Letraset care conţineau pasaje Lorem Ipsum, iar mai recent, prin programele de publicare pentru calculator, ca Aldus PageMaker care includeau versiuni de Lorem Ipsum.
+    </p>
+
+
+    <p>
+        Lorem Ipsum este pur şi simplu o machetă pentru text a industriei tipografice. Lorem Ipsum a fost macheta standard a industriei încă din secolul al XVI-lea, când un tipograf anonim a luat o planşetă de litere şi le-a amestecat pentru a crea o carte demonstrativă pentru literele respective. Nu doar că a supravieţuit timp de cinci secole, dar şi a facut saltul în tipografia electronică practic neschimbată. A fost popularizată în anii '60 odată cu ieşirea colilor Letraset care conţineau pasaje Lorem Ipsum, iar mai recent, prin programele de publicare pentru calculator, ca Aldus PageMaker care includeau versiuni de Lorem Ipsum.
+    </p>
+
 </div>
--- application/views/Students/foreign-interships.php
+++ application/views/Students/foreign-interships.php
@@ -7,20 +7,51 @@
             Facultatea este recunoscuta in UPB prin cel mai mare numar de mobilitati acordate studentilor sai in timpul studiilor. In cei 19 ani de existenta peste 1700 de studenti au fost beneficiari ai unei mobilitati de cel putin 1 luna. Durata cumulata a stagiilor studentilor depaseste 280 de ani.
         </p>
 
-        <div class="clean fi-scolarships">
-            <ul class="clean">
+        <div class="clean fi-tab alfa">
+            <h4 class="clean">Tipuri de stagii</h4>
+
+            <div class="clean internship">
+                <a class="clean" href="#">Stagii de cunoastere a unei intreprinderi: "Decouverte de l'entreprise"</a>
+                <p class="clean">Printre stagiile de acest gen se remarca cu pregnanta cele organizate la Melle (Franta) acolo unde Asociatia "Roumanie Sud-Deux Sèvres" s-a implicat activ in facilitarea contactului studentilor filierei francofone din anul I cu mediul viata si de lucru francez.
+
+                    Asociatia coordonata de dl. Daniel HUBIERE a reusit, incepand cu anul 1995 sa gaseasca parteneri in acest proiect al carui scop este nu numai cunoasterea unui mediu rofesional francofon, dar si punerea in practica a cunostintelor teoretice si lingvistice capatate dupa primul an de studiu in FILS.
+
+                    Stagiarii sunt gazduiti pe parcursul acestei luni stagiu in familii, membre ale Asociatiei ceea ce asigura o experienta de viata inedita si permite un schimb cultural franco-roman extrem de educativ. Studentii descopera cultura si stilul de viata francez, spiritul de voluntariat specific poporului francez, iar gazdele lor au ocazia de percepe Romania din perspective noi.
+
+                    Iata ecourile stagiilor din septembrie 2008 in presa locala:
+
+                    Articol 1
+                    Articol 2
+                    Aducem multumirile noastre sincere Asociatiei "Roumanie Sud-Deux Sèvres" pentru eforturile depuse in cei aproape 15 ani de parteneriat. Stagiile efectuate la Melle reprezinta momente memorabile din viata multor absolventi ai filierei francofone.
+                </p>
+            </div>
+
+
+
+        </div>
+
+        <div class="clean fi-tab lambda">
+            <h4 class="clean">Stagii in Strainatate</h4>
+
+        </div>
+
+        <div class="clean fi-tab omega">
+            <h4 class="clean">Surse de finantare</h4>
+
+        </div>
+
+        <!--
                 <li class="clean"> <a href="#">Burse LLP/Erasmus</a></li>
                 <li class="clean"></i> <a href="#">Burse Ambasada Frantei</a></li>
                 <li class="clean"></i> <a href="#">Burse AUF</a></li>
                 <li class="clean"></i> <a href="#">Burse DAAD</a></li>
                 <li class="clean"></i> <a href="#">Burse private</a></li>
-            </ul>
-        </div>
+-->
 
 
 
 
-
+        <div class="clearfix"></div>
     </div>
 
 </div>
